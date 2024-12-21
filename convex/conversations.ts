b178e739817907e526bc1f53d1bc3db6953e2bc8
Mohammad Munzir <mohammadmunzir2k@gmail.com>
2024-12-21 16:32:57 +0530

    (5th) Get my conversations

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,8 +1,9 @@
 /** @format */
 
-import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
+// Create a new conversation (One to One or Group)
 export const createConversation = mutation({
     args: {
         participants: v.array(v.id("users")),
@@ -49,6 +50,63 @@ export const createConversation = mutation({
     },
 });
 
+// Get my conversations function
+export const getMyConversations = query({
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) throw new ConvexError("Unauthorized");
+
+        const user = await ctx.db
+            .query("users")
+            .withIndex("by_tokenIdentifier", (q) =>
+                q.eq("tokenIdentifier", identity.tokenIdentifier)
+            )
+            .unique();
+
+        if (!user) throw new ConvexError("User not found");
+
+        const conversations = await ctx.db.query("conversations").collect();
+
+        const myConversations = conversations.filter((conversation) => {
+            return conversation.participants.includes(user._id);
+        });
+
+        const conversationWithDetail = await Promise.all(
+            myConversations.map(async (conversation) => {
+                let userDetail = {};
+                if (!conversation.isGroup) {
+                    const otherUserId = conversation.participants.find(
+                        (id) => id !== user._id
+                    );
+                    const userProfile = await ctx.db
+                        .query("users")
+                        .filter((q) => q.eq(q.field("_id"), otherUserId))
+                        .take(1);
+                    userDetail = userProfile[0];
+                }
+
+                const lastMessage = await ctx.db
+                    .query("messages")
+                    .filter((q) =>
+                        q.eq(q.field("conversation"), conversation._id)
+                    )
+                    .order("desc")
+                    .take(1);
+
+                return {
+                    ...userDetail,
+                    ...conversation,
+                    lastMessage: lastMessage[0],
+                };
+            })
+        );
+
+        return conversationWithDetail;
+    },
+});
+
+// Generate upload url function
 export const generateUploadUrl = mutation(async (ctx) => {
     return ctx.storage.generateUploadUrl();
 });
