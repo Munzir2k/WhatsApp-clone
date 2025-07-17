4fb20c1c2a0d9c45fc11ac892d7beff987570c6d
Munzir2k <mohammadmunzir2k@gmail.com>
2025-07-17 12:23:40 +0800

    Changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -118,10 +118,10 @@ export const getRelativeDateTime = (message: any, previousMessage: any) => {
 export function randomID(len: number) {
     let result = "";
     if (result) return result;
-    let chars =
-            "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
-        maxPos = chars.length,
-        i;
+    const chars =
+        "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP";
+    const maxPos = chars.length;
+    let i;
     len = len || 5;
     for (i = 0; i < len; i++) {
         result += chars.charAt(Math.floor(Math.random() * maxPos));
